cf1341d4034df4612a82dce6db05a1ac73da1193
Dylan Bailey <dylancbailey@outlook.com>
2019-02-23 00:02:02 -0500

    Project Initialized

---
+++ src/main/kotlin/com/sirskrubbins/pokemonapimirror/PokemonapimirrorApplication.kt
@@ -0,0 +1,11 @@
+package com.sirskrubbins.pokemonapimirror
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokemonapimirrorApplication
+
+fun main(args: Array<String>) {
+    runApplication<PokemonapimirrorApplication>(*args)
+}
